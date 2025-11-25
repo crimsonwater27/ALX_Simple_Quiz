e78e977cc9480726032f7818970fb3f2ef7061f8
Cole_Yung <utsukushiotaku17@gmail.com>
2025-11-25 23:20:09 +0200

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,23 @@
+function checkAnswer() {
+    const correctAnswer = "4";
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+
+    if (!selectedOption) {
+        document.getElementById('feedback').innerText = "Please select an answer first.";
+        return;
+    }
+
+    const userAnswer = selectedOption.value;
+    const feedback=document.getElementById('feedback');
+
+    if (userAnswer === correctAnswer) {
+        feedback.innerText = "Correct! Well done.";
+        feedback.style.color = "green";
+    } else {
+        feedback.innerText = "Incorrect. Try again!";
+        feedback.style.color = "red";
+    }
+}
+
+const submitButton = document.getElementById('submit-btn');
+submitButton.addEventListener('click', checkAnswer);
